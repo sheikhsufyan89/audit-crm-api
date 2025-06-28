29b6911c31b475666434ddbef577dbe4baed7ee0
sheikhsufyan89 <isheikhsufyan@gmail.com>
2025-06-28 21:05:55 +0500

    Updated user routes.

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -18,12 +18,11 @@ router.post("/login", loginUser);
 router.post("/", createUser);
 router.get("/", getAllUsers);
 
-// Protected routes - example: only authenticated users can access
-router.get("/:id", authenticate, authorizeRoles("admin", "user"), getUserById);
+router.get("/:id", getUserById);
 router.get(
   "/username/:username",
-  authenticate,
-  authorizeRoles("admin", "user"),
+  // authenticate,
+  // authorizeRoles("admin", "user"),
   getUserByUsername
 );
 router.put("/:id", updateUser);
